52eff7de5b720ac076586f211291cce5190c9691
lyizer <1146404200@qq.com>
2024-10-16 12:18:49 +0800

    q

--- server/src/main/java/com/server/server/data/RouteData.java
+++ server/src/main/java/com/server/server/data/RouteData.java
@@ -6,11 +6,19 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 public class RouteData {
+    private double startLat;
+    private double startLong;
+    private double endLat;
+    private double endLong;
     private double distance;  // 距离
     private double duration;  // 时间
     private double price;     // 价格
 
-    public RouteData(double distance, double duration, double price) {
+    public RouteData(double startLat, double startLong, double endLat, double endLong, double distance, double duration, double price) {
+        this.startLat = startLat;
+        this.startLong = startLong;
+        this.endLat = endLat;
+        this.endLong = endLong;
         this.distance = distance;
         this.duration = duration;
         this.price = price;
