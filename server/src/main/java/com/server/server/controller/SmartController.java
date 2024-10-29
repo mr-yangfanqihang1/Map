22f7ee702064ab75909af9a14827a2bac35b8e3b
Komorebiee <135973825+Komorebiee@users.noreply.github.com>
2024-10-29 23:53:08 +0800

    1

--- server/src/main/java/com/server/server/controller/SmartController.java
+++ server/src/main/java/com/server/server/controller/SmartController.java
@@ -2,14 +2,20 @@ package com.server.server.controller;
 
 import com.server.server.service.RouteServiceImpl;
 import com.server.server.service.SmartService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/smart")
 public class SmartController {
+    @Autowired
     private SmartService smartService;
+    @Autowired
     private RouteServiceImpl routeServiceImpl;
+
+    @GetMapping("/getIsStatus")
     public boolean SmartGetIsStatus(){
         boolean isStatus = smartService.getIsStatus();
         routeServiceImpl.setIsStatus();
